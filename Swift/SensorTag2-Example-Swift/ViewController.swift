400a92962537e8cfb480bb8a50766dc0c999c668
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2015-12-21 13:10:28 +0100

    Change project structure to /ObjC/ and /Swift/

---
+++ Swift/SensorTag2-Example-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SensorTag2-Example-Swift
+//
+//  Created by Hans Knöchel on 21.12.15.
+//  Copyright © 2015 Texas Instruments. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
